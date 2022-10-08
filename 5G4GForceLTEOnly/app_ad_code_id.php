ebdcc77ec00a570d5ae8f61e32a8a96a89a6b08a
AlinaFiamze <83856336+AlinaFiamze@users.noreply.github.com>
2022-10-08 12:49:33 +0530

    Update app_ad_code_id.php

--- 5G4GForceLTEOnly/app_ad_code_id.php
+++ 5G4GForceLTEOnly/app_ad_code_id.php
@@ -2,14 +2,14 @@
 	"ad_id":
 		[
 			{
-			"g_ad":"0",
-			"fb_ad":"1",
+			"g_ad":"1",
+			"fb_ad":"0",
 			"adpub_id":"pub-9666127237370790",
 			"adapp_id":"ca-app-pub-9666127237370790~8527728025",
-			"adappopen_id":"ca-app-pub-9666127237370790/3962714280",
+			"adappopen_id":"/21753324030,22764862903/com.alfast.force4g.mode_AppOpen",
 			"adbanner_id":"ca-app-pub-9666127237370790/6970067290",
 			"adbanner_rect_id":"ca-app-pub-9666127237370790/2571657287",
-			"adinterstial_id":"ca-app-pub-9666127237370790/7516862205",
+			"adinterstial_id":"/21753324030,22764862903/com.alfast.force4g.mode_Interstitial",
 			"adnativebanner_id":"ca-app-pub-9666127237370790/5658939330",
 			"adfbbanner_id":"2873867732835565_2873868012835537",
 			"adfbinterstial_id":"2873867732835565_2873868559502149",
